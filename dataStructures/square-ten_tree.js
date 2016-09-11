88db1d728f2f4904407bbbd5c3a281f9034ea345
Cradanimal <cradwimb@gmail.com>
2016-09-10 19:36:16 -0700

    mkdir

---
+++ dataStructures/square-ten_tree.js
@@ -0,0 +1,9 @@
+
+// This one was way too much to write
+// k = level #
+// process follows at level 0 values are 1 to 1 ratio [1,1][2,2]
+// at level 2 = [1, 10^2^k-1], [10^2^k-1 + 1, 2 * 10^2^k-1], [i * 10^2^k-1 + 1, (1 + i) * 10^2^k-1]
+function processData(input) {
+    //Enter your code here
+    
+} 
