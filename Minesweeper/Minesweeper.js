f6bbd3765e915e22159fc05f0d1f3b03c799e6bf
Cradanimal <cradwimb@gmail.com>
2016-10-16 12:30:06 -0700

    clean out old files

--- Minesweeper/Minesweeper.js
+++ Minesweeper/Minesweeper.js
@@ -1,35 +1,35 @@
 $(function() {
-  $(".test").text("Hello World");
+  // $(".test").text("Hello World");
 
 
-  let $form = $("<form></form>");
-  $form.append('<input id="rows" type="number" min="1" max="50" value="10" width="40" size="3" "/>');
-  let $button = $("<input type='button'>")
-  $button.attr('name','Create Grid');
-  $form.append($button);
-  $('body').append($form);
-  $('body').append('<table></table>');
+  // let $form = $("<form></form>");
+  // $form.append('<input id="rows" type="number" min="1" max="50" value="10" width="40" size="3" "/>');
+  // let $button = $("<input type='button'>")
+  // $button.attr('name','Create Grid');
+  // $form.append($button);
+  // $('body').append($form);
+  // $('body').append('<table></table>');
 
 
-  const createGrid = (num) => {
-    for (let i = 0; i < num; i++) {
-      let row = $('<tr></tr>').addClass('row' + i);
-      for (let j = 0; j < num; j++) {
-        let cell = $('<td></td>').addClass('cell ' + j);
-        row.append(cell);
+  // const createGrid = (num) => {
+  //   for (let i = 0; i < num; i++) {
+  //     let row = $('<tr></tr>').addClass('row' + i);
+  //     for (let j = 0; j < num; j++) {
+  //       let cell = $('<td></td>').addClass('cell ' + j);
+  //       row.append(cell);
 
-        cell.click(function() {
-          $(this).hasClass('checked') ? $(this).removeClass('checked') : $(this).addClass('checked');
-        });
-      }
-      $('table').append(row);
-    }
-  }
+  //       cell.click(function() {
+  //         $(this).hasClass('checked') ? $(this).removeClass('checked') : $(this).addClass('checked');
+  //       });
+  //     }
+  //     $('table').append(row);
+  //   }
+  // }
 
 
-  $button.click(function() {
-    let x = $("#rows").val();
-    console.log('hello', x)
-    createGrid(x);
-  })
+  // $button.click(function() {
+  //   let x = $("#rows").val();
+  //   console.log('hello', x)
+  //   createGrid(x);
+  // })
 });
